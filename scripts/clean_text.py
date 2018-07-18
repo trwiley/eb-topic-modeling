6bd4b6c4e6778fff4f33c5abbe03bcad12bfbcb8
trwiley <taylor.r.wiley@gmail.com>
2018-07-18 13:48:08 -0400

    Added comments to the text cleaning script.

--- scripts/clean_text.py
+++ scripts/clean_text.py
@@ -1,10 +1,15 @@
 # clean_text
 # Purpose: clean the Earthbound text dump file.
 
+# load the original file.
 textstream = open("eb_script.txt", "r")
 textdump = textstream.read()
-textdump = textdump.replace("@", "")
 
+# remove '@' and '-' symbols from the text dump
+textdump = textdump.replace("@", "")
+textdump = textdump.replace("-", "")
+
+# write to the new file.
 output = open("eb_script_clean.txt", "w")
 output.write(textdump)
 output.close()
